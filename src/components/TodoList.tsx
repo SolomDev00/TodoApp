d38febfd716debeae4b3bc1c6eaa2bef0fc780c4
SolomDev00 <solomdev00@gmail.com>
2023-12-04 06:14:51 +0200

    Delete Todo Modal

--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -1,4 +1,10 @@
+import { ChangeEvent, FormEvent, useState } from "react";
+import axiosInstance from "../config/axios.config";
+import { ITodo } from "../interfaces";
 import Button from "./schema/Button";
+import Input from "./schema/Input";
+import Modal from "./schema/Modal";
+import Textarea from "./schema/Textarea";
 import Cookies from "universal-cookie";
 import useAuthenticatedQuery from "../hooks/useAuthenticatedQuery";
 
@@ -6,9 +12,18 @@ const TodoList = () => {
   const cookie = new Cookies();
   const userData = cookie.get("userLogged");
 
-  const { isLoading, data } = useAuthenticatedQuery({
-    URL: "/users/me?populate=todos",
-    queryKey: ["todos"],
+  const [isOpenConfirmModal, setIsOpenConfirmModal] = useState(false);
+  const [isEditModalOpen, setIsEditModalOpen] = useState(false);
+  const [isUpdating, setIsUpdating] = useState(false);
+  const [todoToEdit, setTodoToEdit] = useState<ITodo>({
+    id: 0,
+    title: "",
+    description: "",
+  });
+
+  const { data } = useAuthenticatedQuery({
+    queryKey: ["todoList", `${todoToEdit.id}`],
+    URL: "users/me?populate=todos",
     config: {
       headers: {
         Authorization: `Bearer ${userData.jwt}`,
@@ -16,28 +31,162 @@ const TodoList = () => {
     },
   });
 
-  if (isLoading) return <h3>Loading</h3>;
+  // ** Handlers
+  const closeConfirmModal = () => {
+    setTodoToEdit({
+      id: 0,
+      title: "",
+      description: "",
+    });
+    setIsOpenConfirmModal(false);
+  };
+
+  const openConfirmModal = (todo: ITodo) => {
+    setTodoToEdit(todo);
+    setIsOpenConfirmModal(true);
+  };
+
+  const onCloseEditModal = () => {
+    setTodoToEdit({
+      id: 0,
+      title: "",
+      description: "",
+    });
+    setIsEditModalOpen(false);
+  };
+
+  const onOpenEditModal = (todo: ITodo) => {
+    setTodoToEdit(todo);
+    setIsEditModalOpen(true);
+  };
+
+  const onChangeHandler = (
+    evt: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
+  ) => {
+    const { value, name } = evt.target;
+    setTodoToEdit({
+      ...todoToEdit,
+      [name]: value,
+    });
+  };
+
+  const onSubmitRemoveTodo = async () => {
+    try {
+      const { status } = await axiosInstance.delete(`/todos/${todoToEdit.id}`, {
+        headers: {
+          Authorization: `Bearer ${userData.jwt}`,
+        },
+      });
+
+      if (status === 200) {
+        closeConfirmModal();
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const onSubmitUpdateTodo = async (evt: FormEvent<HTMLFormElement>) => {
+    evt.preventDefault();
+    setIsUpdating(true);
+    const { title, description } = todoToEdit;
+    try {
+      const { status } = await axiosInstance.put(
+        `/todos/${todoToEdit.id}`,
+        { data: { title, description } },
+        {
+          headers: {
+            Authorization: `Bearer ${userData.jwt}`,
+          },
+        }
+      );
+
+      if (status === 200) {
+        onCloseEditModal();
+      }
+    } catch (error) {
+      console.log(error);
+    } finally {
+      setIsUpdating(false);
+    }
+  };
 
   return (
-    <div className="space-y-1 ">
-      {data.todos.length ? (
-        data.todos.map((todo) => {
+    <div className="space-y-1">
+      {data && data.length ? (
+        data.map((todo: ITodo) => (
           <div
             key={todo.id}
             className="flex items-center justify-between hover:bg-gray-100 duration-300 p-3 rounded-md even:bg-gray-100"
           >
-            <p className="w-full font-semibold">{todo.title}</p>
+            <p className="w-full font-semibold">
+              {todo.id} - {todo.title}
+            </p>
             <div className="flex items-center justify-end w-full space-x-3">
-              <Button size={"sm"}>Edit</Button>
-              <Button variant={"danger"} size={"sm"}>
+              <Button size={"sm"} onClick={() => onOpenEditModal(todo)}>
+                Edit
+              </Button>
+              <Button
+                variant={"danger"}
+                size={"sm"}
+                onClick={() => openConfirmModal(todo)}
+              >
                 Remove
               </Button>
             </div>
-          </div>;
-        })
+          </div>
+        ))
       ) : (
         <h3>No todos yet!</h3>
       )}
+
+      {/* Edit Todo Modal */}
+      <Modal
+        isOpen={isEditModalOpen}
+        closeModal={onCloseEditModal}
+        title="Edit this todo"
+      >
+        <form className="space-y-3" onSubmit={onSubmitUpdateTodo}>
+          <Input
+            name="title"
+            value={todoToEdit.title}
+            onChange={onChangeHandler}
+          />
+          <Textarea
+            name="description"
+            value={todoToEdit.description}
+            onChange={onChangeHandler}
+          />
+          <div className="flex items-center space-x-3 mt-4">
+            <Button
+              className="bg-indigo-700 hover:bg-indigo-800"
+              isLoading={isUpdating}
+            >
+              Update
+            </Button>
+            <Button variant={"cancel"} onClick={onCloseEditModal}>
+              Cancel
+            </Button>
+          </div>
+        </form>
+      </Modal>
+
+      {/* DELETE TODO CONFIRM MODAL */}
+      <Modal
+        isOpen={isOpenConfirmModal}
+        closeModal={closeConfirmModal}
+        title="Are you sure you want to remove this Todo from your Store?"
+        description="Deleting this Todo will remove it permanently from your inventory. Any associated data, sales history, and other related information will also be deleted. Please make sure this is the intended action."
+      >
+        <div className="flex items-center space-x-3">
+          <Button variant={"danger"} size={"sm"} onClick={onSubmitRemoveTodo}>
+            Yes, remove
+          </Button>
+          <Button variant={"cancel"} size={"sm"} onClick={closeConfirmModal}>
+            Cancel
+          </Button>
+        </div>
+      </Modal>
     </div>
   );
 };
